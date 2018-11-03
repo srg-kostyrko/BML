460d75d3a5d4ee42a107dc0012e82f5e27bb86a2
Sergey Kostyrko <srg.kostyrko@gmail.com>
2018-11-03 21:11:08 +0200

    refactor(tags): Change tags interface to make composition easier
    BREAKING CHANGE: tags with params now should be called like ```array(byte, 1)`name` ``` instead of
    ```array`name`(byte, 1)```

--- __tests__/tags/sequence.js
+++ __tests__/tags/sequence.js
@@ -3,6 +3,6 @@ const { sequence } = require('../../lib/tags/sequence');
 const { uint8, uint16 } = require('../../lib/tags/primitives');
 
 describe('sequence', () => {
-  testTag(sequence`empty`(), [], [], 0);
-  testTag(sequence`mixed`(uint8, uint16), [0x01, 0x00, 0x02], [1, 2], 3);
+  testTag(sequence()`empty`, [], [], 0);
+  testTag(sequence(uint8, uint16)`mixed`, [0x01, 0x00, 0x02], [1, 2], 3);
 });
