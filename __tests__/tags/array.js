7cc12a9d5a5cb9920b97bfd8f87b5ab891fe7f9a
Sergey Kostyrko <srg.kostyrko@gmail.com>
2018-11-06 23:30:31 +0200

    feat(tags): Add bytes alias

--- __tests__/tags/array.js
+++ __tests__/tags/array.js
@@ -1,10 +1,14 @@
 const { testTag } = require('../helpers/tags');
-const { array, byte } = require('../../lib');
+const { array, byte, bytes } = require('../../lib');
 
 describe('array', () => {
   testTag(array(byte, 0)`empty`(), [], [], 0);
   testTag(array(byte, 4)`4 bytes`(), [49, 50, 51, 52], [49, 50, 51, 52], 4);
+  testTag(array(byte, 3)`3 bytes`(), [0x01, 0x02, 0x03], [1, 2, 3], 3);
+});
 
-  const triple = array(byte, 3)`3 bytes`();
-  testTag(triple, [0x01, 0x02, 0x03], [1, 2, 3], 3);
+describe('bytes', () => {
+  testTag(bytes(0)`empty`(), [], [], 0);
+  testTag(bytes(4)`4 bytes`(), [49, 50, 51, 52], [49, 50, 51, 52], 4);
+  testTag(bytes(3)`3 bytes`(), [0x01, 0x02, 0x03], [1, 2, 3], 3);
 });
