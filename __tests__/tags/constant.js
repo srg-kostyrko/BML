af3ee41e5d643a48864dbee863db549551cb3430
Sergey Kostyrko <srg.kostyrko@gmail.com>
2018-11-23 21:13:52 +0200

    feat(constant): Support arrays in constant tag

--- __tests__/tags/constant.js
+++ __tests__/tags/constant.js
@@ -1,8 +1,38 @@
 const { testTag } = require('../helpers/tags');
-const { string, constant, ConstantError } = require('../../lib');
+const {
+  bytes,
+  greedy_bytes,
+  string,
+  constant,
+  ConstantError,
+} = require('../../lib');
 
 describe('constant', () => {
   testTag(constant(string(4), 'RIM4')(), [82, 73, 77, 52], 'RIM4');
+  it('should work with arrays', () => {
+    const matchArray = constant(bytes(4), [1, 2, 3, 4])();
+
+    expect(matchArray.parse(new Uint8Array([1, 2, 3, 4]))).toEqual([
+      1,
+      2,
+      3,
+      4,
+    ]);
+    expect(() =>
+      matchArray.parse(new Uint8Array([82, 73, 77, 53]))
+    ).toThrowError(ConstantError);
+
+    const wrongMatch = constant(greedy_bytes(), [1, 2, 3, 4])();
+
+    expect(() =>
+      wrongMatch.parse(new Uint8Array([1, 2, 3, 4, 5]))
+    ).toThrowError(ConstantError);
+
+    const wrongType = constant(string(4), [82, 73, 77, 52])();
+    expect(() =>
+      wrongType.parse(new Uint8Array([82, 73, 77, 52]))
+    ).toThrowError(ConstantError);
+  });
   it('should throw error if not parsed', () => {
     const magicString = constant(string(4), 'RIM4')();
     expect(() =>
