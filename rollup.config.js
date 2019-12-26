81ca190216594a42067a78700c04c2233b1d97f3
Sergey Kostyrko <srg.kostyrko@gmail.com>
2019-12-26 15:58:02 +0200

    chore: update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
-import json from 'rollup-plugin-json';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 import sourceMaps from 'rollup-plugin-sourcemaps';
 import { terser } from 'rollup-plugin-terser';
 
@@ -11,7 +11,7 @@ const plugins = [
   // Allow json resolution
   json(),
   // Compile TypeScript files
-  typescript({ useTsconfigDeclarationDir: true }),
+  typescript(),
   // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
   commonjs(),
   // Allow node_modules resolution, so you can use 'external' to control
